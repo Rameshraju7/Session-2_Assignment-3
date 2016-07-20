33521a37733baecb7a1dd7a706fd8399a2298a41
Rameshraju7 <mallelaramesh5@gmail.com>
2016-07-20 23:46:59 +0530

    Assignment-3_Pattern
    Session-2_Assignment-3

---
+++ Pattran.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ramesh
+ *
+ */
+public class Pattran {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c,d;
+		for(a=1;a<=5;a++){
+			for(b=1;b<=a;b++){
+				System.out.print(b);
+			}
+			System.out.println();
+		}
+		
+		for(c=4;c>=1;c--){
+			for(d=1;d<=c;d++){
+				System.out.print(d);
+			}
+			System.out.println();
+		}
+		
+	
+	
+}
+}
